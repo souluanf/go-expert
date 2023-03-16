78c5f0b4163ebe2ff5d14778231c1c969033c2a9
Luan Fernandes <souluanf@icloud.com>
2023-03-16 12:44:42 -0300

    Criação de tipos

---
+++ 1.fundacao/3/main.go
@@ -0,0 +1,17 @@
+package main
+
+const a = "Hello World"
+
+type ID int
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Luan"
+	e float64 = 40.0
+	f ID      = 10
+)
+
+func main() {
+	println(f)
+}
