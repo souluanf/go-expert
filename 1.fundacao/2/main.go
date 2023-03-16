44b74a3111d9137c9fb6b7fcfa5e9c7656d19964
Luan Fernandes <souluanf@icloud.com>
2023-03-16 00:55:17 -0300

    Declaração e atribuição

---
+++ 1.fundacao/2/main.go
@@ -0,0 +1,16 @@
+package main
+
+const a = "Hello World"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Luan"
+	e float64 = 40.0
+)
+
+func main() {
+	a := "Valor Inicial"
+	a = "Novo valor"
+	println(a)
+}
