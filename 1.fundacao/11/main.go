662e9b82cc299014a389cfff38c288b56c96c491
Luan Fernandes <souluanf@icloud.com>
2023-03-16 19:28:47 -0300

    Composição de structs

--- 1.fundacao/11/main.go
+++ 1.fundacao/11/main.go
@@ -2,17 +2,22 @@ package main
 
 import "fmt"
 
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
+
 type Cliente struct {
 	Nome  string
 	Idade int
 	Ativo bool
+	Endereco
 }
 
 func main() {
-	luan := Cliente{
-		Nome:  "Luan",
-		Idade: 28,
-		Ativo: false,
-	}
+	luan := Cliente{Nome: "Luan", Idade: 28, Ativo: false}
+	luan.Endereco = Endereco{Logradouro: "Rua Teste", Numero: 45, Cidade: "São Paulo", Estado: "SP"}
 	fmt.Println(luan)
 }
