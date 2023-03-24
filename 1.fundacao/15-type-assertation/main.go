5245550da170a0b78aa7f0791b025953a397a93a
Luan Fernandes <souluanf@icloud.com>
2023-03-24 00:18:01 -0300

    type assertation

---
+++ 1.fundacao/15-type-assertation/main.go
@@ -0,0 +1,13 @@
+package main
+
+
+func main() {
+	var minhaVar interface{} = "Luan Fernandes"
+	println(minhaVar.(string)) // afirmando para sistema que é uma String
+	resultsdo, ok := minhaVar.(int)
+	if ok {
+		println(resultsdo)
+	} else {
+		println("Não é um inteiro")
+	}
+}
