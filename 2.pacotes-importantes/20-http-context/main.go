6661606b37e2139c960c8a9c53d25ed7c0cd42f8
Luan Fernandes <souluanf@icloud.com>
2023-03-24 22:14:16 -0300

    http context

---
+++ 2.pacotes-importantes/20-http-context/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://www.google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		println(string(body))
+	}
+}
