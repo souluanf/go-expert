d32593122a5634cd756ffc01f55486e86bc35d51
Luan Fernandes <souluanf@icloud.com>
2023-03-29 22:10:09 -0300

    verificando cobertura

---
+++ 6-testing/3-verificando-cobertura/go.mod
@@ -0,0 +1,3 @@
+module github.com/souluanf/go-expert/6-testing/3-verificando-cobertura
+
+go 1.20
---
+++ 6-testing/3-verificando-cobertura/tax.go
@@ -0,0 +1,11 @@
+package tax
+
+func CalculateTax(amount float64) float64 {
+	if amount == 0 {
+		return 0
+	}
+	if amount < 1000 {
+		return 0.05
+	}
+	return 0.1
+}
