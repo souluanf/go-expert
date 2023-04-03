cddcb225dbfbd72683a905ac30664be1344a4ad8
Luan Fernandes <souluanf@icloud.com>
2023-04-03 09:52:09 -0300

    finalizando teste user_db

--- 7-apis/internal/infra/database/user_db_test.go
+++ 7-apis/internal/infra/database/user_db_test.go
@@ -27,5 +27,25 @@ func TestUser_CreateUser(t *testing.T) {
 	assert.Equal(t, user.Name, userFound.Name)
 	assert.Equal(t, user.Email, userFound.Email)
 	assert.NotNilf(t, user.Password, userFound.Password)
+}
 
+func TestUser_FindUserByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("Luan Fernandes", "souluanf@icloud.com", "123456")
+	userDB := NewUser(db)
+
+	err = userDB.CreateUser(user)
+	assert.Nil(t, err)
+
+	var userFound entity.User
+	err = db.First(&userFound, "email = ?", user.Email).Error
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNilf(t, user.Password, userFound.Password)
 }
