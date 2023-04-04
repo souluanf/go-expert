67a606fdbac52713fc8647eb80553608cd2c3d01
Luan Fernandes <souluanf@icloud.com>
2023-04-04 13:25:25 -0300

    buscando e alterando products

--- 7-apis/cmd/server/main.go
+++ 7-apis/cmd/server/main.go
@@ -32,6 +32,8 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
 	err = http.ListenAndServe("localhost:8080", r)
 	if err != nil {
 		panic(err)
