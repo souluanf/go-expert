2e791facd8d015062da6d25fec88207da9aa621a
Luan Fernandes <souluanf@icloud.com>
2023-07-11 23:12:03 -0300

    feat: go routines

---
+++ 8-multithreading/1-go-routines/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(name string) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d: Task %s is running\n", i, name)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+// Thread 1
+func main() {
+	// Thread 2
+	go task("A")
+	// Thread 3
+	go task("B")
+	// Thread 4
+	go func() {
+		for i := 0; i < 5; i++ {
+			fmt.Printf("%d: Task %s is running\n", i, "anonymous")
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+	time.Sleep(15 * time.Second)
+}
