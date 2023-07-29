42425f03d49c87a90969db29c6364d53085a0943
Luan Fernandes <luan@companyhero.com>
2023-07-29 00:52:42 -0300

    feat: Range With WaitGroup

---
+++ 8-multithreading/4-channels/4-range-with-waitgroup/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	ch := make(chan int)
+
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+
+	go publish(ch)
+	go reader(ch, &wg)
+	wg.Wait()
+}
+
+func reader(ch chan int, wg *sync.WaitGroup) {
+	for v := range ch {
+		fmt.Printf("Received: %d\n", v)
+		wg.Done()
+	}
+}
+
+func publish(ch chan int) {
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+}
