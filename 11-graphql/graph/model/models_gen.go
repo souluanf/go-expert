b4ece93d59ff314abed8e2c29ff710ffdd3dc1ea
Luan Fernandes <hello@luanfernandes.dev>
2023-09-19 00:16:39 -0300

    feat: graphql module

--- 11-graphql/graph/model/models_gen.go
+++ 11-graphql/graph/model/models_gen.go
@@ -2,27 +2,13 @@
 
 package model
 
-type Category struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description *string   `json:"description,omitempty"`
-	Courses     []*Course `json:"courses"`
-}
-
-type Course struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description *string   `json:"description,omitempty"`
-	Category    *Category `json:"category"`
-}
-
 type NewCategory struct {
 	Name        string  `json:"name"`
-	Description *string `json:"description,omitempty"`
+	Description *string `json:"description"`
 }
 
 type NewCourse struct {
 	Name        string  `json:"name"`
-	Description *string `json:"description,omitempty"`
+	Description *string `json:"description"`
 	CategoryID  string  `json:"categoryId"`
 }
--- 11-graphql/graph/resolver.go
+++ 11-graphql/graph/resolver.go
@@ -8,4 +8,5 @@ import "github.com/souluanf/fullcycle-graphql/internal/database"
 
 type Resolver struct {
 	CategoryDB *database.Category
+	CourseDB   *database.Course
 }
