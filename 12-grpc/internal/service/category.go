fc0bce72ab92b6783d022fc9316434a356120fef
Luan Fernandes <hello@luanfernandes.dev>
2023-09-20 23:13:45 -0300

    feat: Listando categories

--- 12-grpc/internal/service/category.go
+++ 12-grpc/internal/service/category.go
@@ -29,3 +29,19 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 
 	return categoryResponse, nil
 }
+
+func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+	var categoryResponses []*pb.Category
+	for _, category := range categories {
+		categoryResponses = append(categoryResponses, &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		})
+	}
+	return &pb.CategoryList{Categories: categoryResponses}, nil
+}
