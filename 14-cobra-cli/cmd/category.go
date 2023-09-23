486a9bac3034e8daffb7d95d8337d1ea21296658
Luan Fernandes <hello@luanfernandes.dev>
2023-09-23 16:00:22 -0300

    feat: lags mudando valor por referência

--- 14-cobra-cli/cmd/category.go
+++ 14-cobra-cli/cmd/category.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -19,28 +16,19 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		name, _ := cmd.Flags().GetString("name")
 		exists, _ := cmd.Flags().GetBool("exists")
 		id, _ := cmd.Flags().GetInt16("id")
-		fmt.Println("category called", name)
+		fmt.Println("category called", category)
 		fmt.Println("exists", exists)
 		fmt.Println("id", id)
 	},
 }
 
+var category string
+
 func init() {
 	rootCmd.AddCommand(categoryCmd)
-	categoryCmd.PersistentFlags().StringP("name", "n", "Y", "Name of category")
+	categoryCmd.PersistentFlags().StringVarP(&category, "name", "n", "", "Name of category")
 	categoryCmd.PersistentFlags().BoolP("exists", "e", false, "Check if category exists")
 	categoryCmd.PersistentFlags().Int16P("id", "i", 0, "ID of category")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// categoryCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// categoryCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
